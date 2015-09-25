d653debff50d64aa3c0d7a78a0b87ac30245e5a4
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-09-25 14:57:32 +0100

    remove flask server for now

--- src/main.py
+++ src/main.py
@@ -1,9 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello resin sync!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+print 'Im a new commit'
